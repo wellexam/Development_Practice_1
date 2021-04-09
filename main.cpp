65b1263506dbd077245cc711a405c5e013f2634e
ZhaoWenYe <3136685582@qq.com>
2021-04-09 13:20:26 +0800

    basic input function with error

--- main.cpp
+++ main.cpp
@@ -6,22 +6,50 @@ using namespace std;
 
 class object
 {
-    int OBJECTID;
-    int pointid;
-    float grid_code;
-    float Lon;
-    float Lat;
+    public:
+        int pointid;
+        double grid_code;
+        double Lon;
+        double Lat;
 };
 
 int main()
 {
+    object data[120000];
     fstream in;
-    char temp[256];
-    in.open("\\DEM_TXT\\1.txt", ios::in);
+    char head[256];
+    in.open("DEM_TXT\\1.txt", ios::in);
+    in.getline(head, 256, '\n');
+    int count = 0;
+    int id;
+    double digit;
+    char trash;
     while(!in.eof())
     {
-        in.getline(temp, 256, '\n');
-        cout << temp << endl;
+        in >> id;
+        in >> trash;
+        in >> id;
+        data[count].pointid = id;
+        cout << data[count].pointid<< ' ';
+        in >> trash;
+        in >> digit;
+        data[count].grid_code = digit;
+        cout << digit << " ";
+        cout << data[count].grid_code<< ' ';
+        in >> trash;
+        in >> digit;
+        data[count].Lon = digit;
+        cout << data[count].Lon<< ' ';
+        in >> trash;
+        in >> digit;
+        data[count].Lat = digit;
+        cout << data[count].Lat<< endl;
+        // in >> trash;
+        count++;
+        if(count >= 100)
+        {
+            break;
+        }
     }
     in.close();
     return 0;
