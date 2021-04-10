6810cd8bcbab2fb72581a9c845e614be6d4a6d5b
wellexam <el.zhao.me@gmail.com>
2021-04-10 20:45:04 +0800

    Essential part finished.

--- main.cpp
+++ main.cpp
@@ -12,32 +12,31 @@ class object
         long double grid_code;
         long double Lon;
         long double Lat;
-        static long double LatMax, LatMin, LonMax, LonMin;
-        // static bool ValueCmp(object const &a, object const &b)
-        // {
-        //     return a.grid_code > b.grid_code;
-        // }
-        // friend bool operator > (const object &a, const object &b)
-        // {
-        //     return a.grid_code > b.grid_code;
-        // }
-        // friend bool operator == (const object &a, const object &b)
-        // {
-        //     return a.grid_code == b.grid_code;
-        // }
-        // friend bool operator != (const object &a, const object &b)
-        // {
-        //     return a.grid_code != b.grid_code;
-        // }
-        // friend object operator - (const object &a, const object &b)
-        // {
-        //     object temp;
-        //     temp.grid_code = a.grid_code - b.grid_code;
-        //     temp.pointid = a.pointid;
-        //     temp.Lon = a.Lon;
-        //     temp.Lat = a.Lat;
-        //     return temp;
-        // }
+        friend bool operator > (const object &a, const object &b)
+        {
+            return a.grid_code < b.grid_code;
+        }
+        friend bool operator < (const object &a, const object &b)
+        {
+            return a.grid_code > b.grid_code;
+        }
+        friend bool operator == (const object &a, const object &b)
+        {
+            return a.grid_code == b.grid_code;
+        }
+        friend bool operator != (const object &a, const object &b)
+        {
+            return a.grid_code != b.grid_code;
+        }
+        friend object operator - (const object &a, const object &b)
+        {
+            object temp;
+            temp.grid_code = a.grid_code - b.grid_code;
+            temp.pointid = a.pointid;
+            temp.Lon = a.Lon;
+            temp.Lat = a.Lat;
+            return temp;
+        }
 };
 
 
@@ -47,55 +46,91 @@ int main()
     static object data[120000];
     fstream in;
     char head[256];
-    in.open("DEM_TXT\\1.txt", ios::in);
+    in.open("DEM_TXT\\2.txt", ios::in);
     in.getline(head, 256, '\n');
     int count = 0;
     int id;
-    char trash;
+    char comma;
+    long double digit;
+
+    in >> id;
+    in >> comma;//逗号
+    in >> id;
+    data[count].pointid = id;
+    in >> comma;//逗号
+    in >> digit;
+    data[count].grid_code = digit;
+    in >> comma;//逗号
+    in >> digit;
+    data[count].Lon = digit;
+    in >> comma;//逗号
+    in >> digit;
+    data[count].Lat = digit;
+
+    long double LatMax=data[count].Lat, LatMin=data[count].Lat, LonMax=data[count].Lon, LonMin=data[count].Lon;
+    count++;
+
     while(!in.eof())
     {
-        long double digit;
+        // long double digit;
         in >> id;
-        in >> trash;
+        in >> comma;//逗号
 
         in >> id;
         data[count].pointid = id;
-        // cout << data[count].pointid<< ' ';
 
-        in >> trash;
+        in >> comma;//逗号
+
         in >> digit;
         data[count].grid_code = digit;
-        // cout << digit << " ";
-        std::cout << std::fixed << std::setprecision(7) << data[count].grid_code<< ' ';
 
-        in >> trash;
+        in >> comma;//逗号
+
         in >> digit;
         data[count].Lon = digit;
-        // cout << data[count].Lon<< ' ';
 
-        in >> trash;
+        in >> comma;//逗号
+
         in >> digit;
         data[count].Lat = digit;
-        // cout << data[count].Lat<< endl;
+        if(LatMax<data[count].Lat)
+        {
+            LatMax=data[count].Lat;
+        }
+        if(LatMin>data[count].Lat)
+        {
+            LatMin=data[count].Lat;
+        }
+        if(LonMax<data[count].Lon)
+        {
+            LonMax=data[count].Lon;
+        }
+        if(LonMin>data[count].Lon)
+        {
+            LonMin=data[count].Lon;
+        }
 
         count++;
-        if(count >= 100)
-        {
-            break;
-        }
+        // if(count >= 100)
+        // {
+        //     break;
+        // }
     }
     count--;
-    cout << endl;
-    sort(data[0], data[count],
-          [](object const & a, object const & b) -> bool
-          { return a.grid_code < b.grid_code; } );
-    for (int i = 0; i<count; i++)
+    sort(data, data + count);
+    for (int i = 0; i<20; i++)
     {
-        cout << data[i].pointid<< ' ';
-        cout << fixed << setprecision(7) << data[i].grid_code<< ' ';
-        cout << data[i].Lon<< ' ';
-        cout << data[i].Lat<< endl;
+        cout << data[i].pointid << ' '
+             << fixed << setprecision(7)
+             << data[i].grid_code << ' '
+             << data[i].Lon << ' '
+             << data[i].Lat << endl;
     }
+    cout << fixed << setprecision(7)
+         << "LatMax == " << LatMax << " "
+         << "LatMin == " << LatMin << " "
+         << "LonMax == " << LonMax << " "
+         << "LonMin == " << LonMin << endl;
     in.close();
     return 0;
 }
